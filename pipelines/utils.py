68b94b274856a785f76e007668f525d396030f3c
luke-strange <92686634+luke-strange@users.noreply.github.com>
2024-10-04 17:00:06 +0100

    Update utils.py

--- pipelines/utils.py
+++ pipelines/utils.py
@@ -4,49 +4,10 @@ import zipfile
 import os
 import time
 import numpy as np
-import geopandas as gpd
-from shapely.geometry import Point
 
 class BusDetail:
     def __init__(self) -> None:
         pass
-    # def descri():
-    #     print("")
-
-# QUERY = """
-# CREATE TABLE test AS
-# SELECT 
-#     * 
-# FROM 
-#     agencies as t1
-# JOIN 
-#     routes as t2 ON t1.agency_id = t2.agency_id
-# JOIN
-#     trips as t3 ON t2.route_id = t3.route_id
-# JOIN
-#     stop_times as t4 ON t4.trip_id = t3.trip_id
-# JOIN
-#     stops as t5 ON t5.stop_id = t4.stop_id;
-# """
-# STOP_QUERY = """
-# SELECT 
-#     * 
-# FROM 
-#     stop_times as t1
-# JOIN
-#     stops as t2 ON t2.stop_id = t1.stop_id;
-# """
-# AGGREGATE_STOPS = """
-# SELECT 
-#     trip_id,
-#     ARRAY_AGG(stop_id) AS stop_ids,
-#     ARRAY_AGG(stop_lat) AS stop_lats,
-#     ARRAY_AGG(stop_lon) AS stop_lons
-# FROM
-#     stops
-# GROUP BY 
-#     trip_id;
-# """
 
 def haversine(lat1, lon1, lat2, lon2):
     # Convert degrees to radians
@@ -60,4 +21,118 @@ def haversine(lat1, lon1, lat2, lon2):
 
     # Earth radius in kilometers
     r = 6371
-    return c * r
+    return round(c * r, 5)
+
+def unzip_file(zip_file_path, extract_dir):
+    # Open the zip file
+    with zipfile.ZipFile(zip_file_path, 'r') as zip:
+        zip.extractall(extract_dir)
+        file_paths = zip.namelist()
+        
+        # Go through each file in the zip archive
+        for file_name in file_paths:
+            # Construct the full file path of the extracted file
+            full_file_path = os.path.join(extract_dir, file_name)
+    print(f"Successfully unzipped all files in {zip_file_path} to {extract_dir}.")
+    return
+
+def load_gtfs_ids(gtfs_path):
+    """
+    Given an extracted folder of GTFS files, load the parts containing IDs into
+    pandas dataframes using read_csv.
+
+    Parameters
+    -------
+    gtfs_path: str
+        The directory containing the extracted text files from the GTFS timetable
+
+    Returns
+    -------
+    agencies: Pandas.DataFrame
+        Contains "agency_id" and "agency_noc" columns.
+
+    routes: Pandas.DataFrame
+        Contains 'agency_id', 'route_id', 'route_short_name', 'route_type' columns.
+
+    stops: Pandas.DataFrame
+        Contains 'stop_id', 'stop_lat', 'stop_lon' columns.
+
+    stop_times: Pandas.DataFrame
+        Contains 'trip_id', 'stop_id' columns.
+    
+    trips: Pandas.DataFrame
+        Contains 'trip_id', 'route_id' columns.
+    """
+    for file in os.listdir(gtfs_path):
+        fp = os.path.join(gtfs_path, file)
+        data = pd.read_csv(fp, low_memory=False)
+        
+        if file == 'agency.txt':
+            agencies = data[['agency_id', 'agency_noc']]
+            print('Loaded agency.txt')
+
+        if file == 'stops.txt':
+            stops = data[['stop_id', 'stop_lat', 'stop_lon']]
+            print('Loaded stops.txt')
+
+        if file == 'stop_times.txt':
+            stop_times = data[['trip_id', 'stop_id']]
+            print('Loaded stop_times.txt')
+
+        if file == 'trips.txt':
+            trips = data[['trip_id', 'route_id']]
+            print('Loaded trips.txt')
+
+        # if file == 'calendar.txt':
+        #     calendar = data
+        #     print('Loaded calendar.txt')
+        # if file == 'calendar_dates.txt':
+        #     calendar_dates = data
+        #     print('Loaded calendar_dates.txt')
+
+        if file == 'routes.txt':
+            routes = data[['agency_id', 'route_id', 'route_short_name', 'route_type']]
+            print('Loaded routes.txt')
+    
+    return agencies, routes, stops, stop_times, trips
+
+def load_full_gtfs(dir):
+    files_in_path = os.listdir(dir)
+    required_files = ['agency.txt', 'routes.txt', 'trips.txt', 'stops.txt', 'stop_times.txt', 'calendar.txt' 'calendar_dates.txt']
+    for file in required_files:
+        assert file in files_in_path, f"The file {file} is required but is not present in the folder directory {dir}"
+        fp = os.path.join(dir, file)
+        data = pd.read_csv(fp, low_memory=False)
+        if file == 'agency.txt':
+            agencies = data
+        if file == 'stops.txt':
+            stops = data
+        if file == 'stop_times.txt':
+            stop_times = data
+        if file == 'trips.txt':
+            trips = data
+        if file == 'calendar.txt':
+            calendar = data
+        if file == 'calendar_dates.txt':
+            calendar_dates = data
+        if file == 'routes.txt':
+            routes = data
+    
+    result = [agencies, routes, trips, stops, stop_times, calendar, calendar_dates]
+
+    if len(files_in_path) > len(required_files):
+        print('Loading additional non-required files.')
+        for file in files_in_path and file not in required_files:
+            if file == 'feed_info.txt':
+                feed_info = data
+                result.append(feed_info)
+            if file == 'frequencies.txt':
+                frequencies = data
+                result.append(frequencies)
+            if file == 'shapes.txt':
+                shapes = data
+                result.append(shapes)
+    
+    return result
+        
+    
