72795af493ecfa13ef0b6e965aed16ac92398d80
luke-strange <92686634+luke-strange@users.noreply.github.com>
2024-10-15 15:10:09 +0100

    Add isochrone script

---
+++ R/README.md
@@ -0,0 +1,7 @@
+# How to make travel time isochrones
+
+You need a an osm.pbf file for the area in question. A good place to look is the [geofabrik website](https://download.geofabrik.de/).
+
+You also need a GTFS timetable file.
+
+Ensure these are in your `data_path` which you will set in `make_isochrones.R`.
---
+++ R/make_iscochrones.R
@@ -0,0 +1,43 @@
+# setwd("/Users/lukestrange/Code/bus-tracking/R")
+# Increase memory
+options(java.parameters = "-Xmx8G")
+
+# Check if 'r5r' is installed, and install it if not
+# if (!requireNamespace("r5r", quietly = TRUE)) {
+#   remotes::install_github("ipeaGIT/r5r")
+# }
+library(r5r)
+library(sf)
+library(ggplot2)
+
+# data_path <- system.file("/Users/lukestrange/Code/bus-tracking/R/data", package = 'r5r')
+# print(list.files(data_path))
+
+# # Indicate the path where OSM and GTFS data are stored
+r5r_core <- setup_r5(data_path = "/Users/lukestrange/Code/bus-tracking/R/timetable")
+
+origin <- data.frame(id='origin1', lon=-1.54702, lat=53.79955)
+
+departure_datetime <- as.POSIXct(
+  "19-09-2024 09:00:00",
+  format = "%d-%m-%Y %H:%M:%S"
+)
+
+iso1 <- isochrone(r5r_core, 
+                  origins = origin, 
+                  mode="TRANSIT", 
+                  mode_egress="WALK",
+                  max_walk_time=5, 
+                  departure_datetime = departure_datetime, 
+                  cutoffs = c(0, 2700))
+
+head(iso1)
+q
+colour <- c('#ffe0a5', '#003f5c')
+
+# plot(iso1['isochrone'], col = colour[nrow(iso1['isochrone'])])
+
+# Save as GeoJSON
+st_write(iso1['isochrone'], "timetable_19092924_0900.geojson", driver = "GeoJSON")
+
+stop_r5(r5r_core)
