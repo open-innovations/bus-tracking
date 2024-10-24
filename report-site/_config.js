89042fdc459914abf2b1bca6cb832e851cd089bf
luke-strange <92686634+luke-strange@users.noreply.github.com>
2024-10-24 17:36:23 +0100

    Update to lume viz 0.16.4

--- report-site/_config.js
+++ report-site/_config.js
@@ -8,7 +8,7 @@ import nav from "lume/plugins/nav.ts";
 import postcss from "lume/plugins/postcss.ts";
 
 // Importing the OI Lume charts and utilities
-import oiViz from "https://deno.land/x/oi_lume_viz@v0.16.4/mod.ts";
+import oiViz from "https://deno.land/x/oi_lume_viz@v0.16.5/mod.ts";
 import autoDependency from "https://deno.land/x/oi_lume_utils@v0.4.0/processors/auto-dependency.ts";
 import csvLoader from "https://deno.land/x/oi_lume_utils@v0.4.0/loaders/csv-loader.ts";
 
